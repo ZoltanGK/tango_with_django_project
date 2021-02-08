dace6bdaa3a504210bdc601a7849fbd7c3e7c398
Zoltan Kiss <2479436k@student.gla.ac.uk>
2021-02-08 17:11:24 +0100

    Chapter 9 End: Added further restrictions based on user login status

--- rango/views.py
+++ rango/views.py
@@ -37,6 +37,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -48,6 +49,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -133,7 +135,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
